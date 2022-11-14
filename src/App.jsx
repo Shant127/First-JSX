dc709c24a5e61f5141f79da35f71dc274869e52c
Shant S <110202516+Shant127@users.noreply.github.com>
2022-11-14 12:25:20 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from './components/Hello';
+import './App.css';
+
+function App() {
+  return (
+    <fieldset>
+      <legend>App.jsx</legend>
+      <Hello />
+    </fieldset>
+  );
+}
+
+export default App;
